0f08059f14d5dceebdc76c0af677a0eb3e6d5842
Gvde2 <153321853+Gvde2@users.noreply.github.com>
2023-12-08 12:46:09 -0800

    Upload Files

---
+++ PUBG Mobile HKandBP/ESP/pch.h
@@ -0,0 +1,37 @@
+﻿ 
+#ifndef PCH_H
+#define PCH_H
+
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+#pragma comment(lib,"Dwmapi.lib")
+
+#include <Windows.h>
+#include <dwmapi.h>
+
+#include <TlHelp32.h>
+#include <tchar.h>
+ 
+#include <string>
+#include <iostream>
+#include <winternl.h>
+#include <ntstatus.h>
+#include <iomanip> 
+#pragma comment (lib, "ntdll.lib")
+
+
+
+#include "termcolor.h" 
+
+#include "ProcManager.h" 
+#include "MemoryManager.h"
+
+#include "Overlay.h"
+
+#include "Esp.h"
+
+
+
+#endif //PCH_H
