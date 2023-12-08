7e45a72ecf06fd71ab80f2868a4ede785a8750c8
Gvde2 <153321853+Gvde2@users.noreply.github.com>
2023-12-08 12:45:54 -0800

    Upload Files

---
+++ PUBG Mobile HKandBP/ESP/EagleEye.cpp
@@ -0,0 +1,57 @@
+﻿ 
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+
+
+
+	int gamePid = ProcManager::getAowProcID();
+	 
+
+	g_pMemoryManager->init(gamePid, true);
+
+
+	g_pEsp->init();
+
+
+
+	g_pOverlay->init();
+
+
+
+
+	sf::Vertex line[2];
+	line[0].color = sf::Color::Red;
+	line[1].color = sf::Color::Green;
+	line[0].position = sf::Vector2f(0,0);
+	line[1].position = sf::Vector2f(500,500);
+
+
+
+	while(g_pOverlay->window.isOpen()){
+
+		
+		g_pOverlay->window.clear(sf::Color::Transparent);
+
+
+		/* DRAW */
+
+		g_pOverlay->window.draw(line, 2, sf::Lines);
+
+
+		g_pOverlay->window.display();
+
+
+	}
+
+
+
+
+
+
+    std::cout << "Hello World!\n"; 
+}
+ 
